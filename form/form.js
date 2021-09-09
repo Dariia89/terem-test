c75c579b0f5704cae0d9d98a3a57c5a891829e7b
Dariia89 <alefed89@mail.ru>
2021-09-10 01:01:22 +0300

    efforts to solve 2.5

--- form/form.js
+++ form/form.js
@@ -1,4 +1,34 @@
 // форма
+
+// задание 2.6 я не совсем поняла, поэтому просто оставила свои тщетные попытки разобраться в комментарии
+// сразу прошу прощения, если делаю совсем не то
+
+// const getData = async () => {
+//     // let xhr = new XMLHttpRequest();
+//     // xhr.open('GET', './test.json', true);
+//     // xhr.onreadystatechange = () => {
+//     //     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+//     //         alert('Great!');
+//     //     } else {
+//     //         alert('Fail!');
+//     //     }
+//     // }
+//     // xhr.send();
+
+
+//  или
+
+
+//     // await fetch('./test.json').then(response => {
+//     //     if (response.ok) {
+//     //         alert('Great');
+//     //     } else {
+//     //         alert('Fail');
+//     //     }
+//     // })
+// }
+
+//2.5
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('#form');
     form.addEventListener('submit', async function formSend(e) {
@@ -16,5 +46,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
         dataBlock.style.visibility = 'visible';
         dataBlock.innerHTML = `${json}`;
+
+        // getData();
     });
 });
+
+
